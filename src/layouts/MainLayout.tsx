5b02e7484b718e9e8b0b39631c3925e462f0731b
DontCryG <tanattapat1996@gmail.com>
2026-08-27 19:06:43 +0700

    fix: hide Header and Sidebar during print

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -9,19 +9,23 @@ export default function MainLayout() {
   
   return (
     <div className="flex h-screen bg-slate-50 overflow-hidden text-slate-800">
-      <Sidebar 
-        isMobileMenuOpen={isMobileMenuOpen}
-        setIsMobileMenuOpen={setIsMobileMenuOpen}
-        isCollapsed={isSidebarCollapsed}
-        setIsCollapsed={setIsSidebarCollapsed}
-      />
+      <div className="print:hidden">
+        <Sidebar 
+          isMobileMenuOpen={isMobileMenuOpen}
+          setIsMobileMenuOpen={setIsMobileMenuOpen}
+          isCollapsed={isSidebarCollapsed}
+          setIsCollapsed={setIsSidebarCollapsed}
+        />
+      </div>
 
       <main className="flex-1 flex flex-col min-w-0 overflow-hidden relative">
-        <Header 
-          setIsMobileMenuOpen={setIsMobileMenuOpen}
-          isSidebarCollapsed={isSidebarCollapsed}
-          setIsSidebarCollapsed={setIsSidebarCollapsed}
-        />
+        <div className="print:hidden">
+          <Header 
+            setIsMobileMenuOpen={setIsMobileMenuOpen}
+            isSidebarCollapsed={isSidebarCollapsed}
+            setIsSidebarCollapsed={setIsSidebarCollapsed}
+          />
+        </div>
         
         <div className="flex-1 overflow-auto p-4 md:p-6 lg:p-8 relative">
           
